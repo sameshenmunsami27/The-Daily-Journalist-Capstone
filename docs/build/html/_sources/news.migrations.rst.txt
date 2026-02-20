168760017e35eff748730e2b8c673ce58cbdd387
Sameshen Munsami <sameshenmunsami27@gmail.com>
2026-02-20 16:59:11 +0200

    Commit all documentation changes to docs branch

---
+++ docs/build/html/_sources/news.migrations.rst.txt
@@ -0,0 +1,69 @@
+news.migrations package
+=======================
+
+Submodules
+----------
+
+news.migrations.0001\_initial module
+------------------------------------
+
+.. automodule:: news.migrations.0001_initial
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+news.migrations.0002\_alter\_article\_author\_alter\_article\_publisher\_and\_more module
+-----------------------------------------------------------------------------------------
+
+.. automodule:: news.migrations.0002_alter_article_author_alter_article_publisher_and_more
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+news.migrations.0003\_alter\_article\_author\_alter\_newsletter\_author module
+------------------------------------------------------------------------------
+
+.. automodule:: news.migrations.0003_alter_article_author_alter_newsletter_author
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+news.migrations.0004\_remove\_publisher\_editors\_and\_more module
+------------------------------------------------------------------
+
+.. automodule:: news.migrations.0004_remove_publisher_editors_and_more
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+news.migrations.0005\_newsletter module
+---------------------------------------
+
+.. automodule:: news.migrations.0005_newsletter
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+news.migrations.0006\_remove\_user\_journalist\_followers\_and\_more module
+---------------------------------------------------------------------------
+
+.. automodule:: news.migrations.0006_remove_user_journalist_followers_and_more
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+news.migrations.0007\_alter\_article\_options module
+----------------------------------------------------
+
+.. automodule:: news.migrations.0007_alter_article_options
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: news.migrations
+   :members:
+   :show-inheritance:
+   :undoc-members:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,17 +1,46 @@
 import os
 import sys
 import django
+from django.conf import settings
+from django.core.management import call_command
+from unittest.mock import MagicMock
 
 # Path setup 
-# We point Sphinx to the root 'News Application' folder
 sys.path.insert(0, os.path.abspath('../..'))
 
-# Tells Sphinx where your Django settings are located
-os.environ['DJANGO_SETTINGS_MODULE'] = 'news_project.settings'
+# --- THE "STRICT" BYPASS START ---
+if not settings.configured:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'news_project.settings'
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:', # Temporary database in RAM
+            }
+        },
+        INSTALLED_APPS=[
+            'django.contrib.contenttypes',
+            'django.contrib.auth',
+            'django.contrib.admin',
+            'django.contrib.sessions',
+            'django.contrib.messages',
+            'news', 
+        ],
+        SECRET_KEY='docs-build-key',
+    )
+    django.setup()
+    
+    # THIS IS THE KEY: Create the tables in the dummy database
+    try:
+        call_command('migrate', verbosity=0, interactive=False)
+    except Exception:
+        pass
 
-# This initializes Django so Sphinx can read the models and MySQL setup
-django.setup()
-
+# Mocking migrations to prevent further DB issues
+class MockMigrator:
+    def __getattr__(self, name): return MagicMock()
+sys.modules['django.db.migrations'] = MockMigrator()
+# --- THE "STRICT" BYPASS END ---
 
 # Project information
 project = 'The Daily Journalist'
@@ -19,8 +48,7 @@ copyright = '2026, Sameshen Munsami'
 author = 'Sameshen'
 release = '1.0.0'
 
-
-#  General configuration 
+# General configuration 
 extensions = [
     'sphinx.ext.autodoc',      # Core library to grab docstrings
     'sphinx.ext.viewcode',     # Adds links to your highlighted source code
@@ -29,10 +57,25 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv', 'venv']
+exclude_patterns = [
+    '_build', 'Thumbs.db', '.DS_Store', 
+    '.venv', 'venv', '**/migrations/*', 'lib'
+]
 
-
-#  Options for HTML output 
-# We use the 'Read the Docs' theme for a professional look
+# Options for HTML output 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Fix for Python 3.13 / Sphinx ValueError
+def setup(app):
+    from sphinx.util import inspect
+    
+    orig_object_description = inspect.object_description
+    
+    def patched_object_description(obj, *args, **kwargs):
+        try:
+            return orig_object_description(obj, *args, **kwargs)
+        except (ValueError, TypeError):
+            return repr(obj)
+            
+    inspect.object_description = patched_object_description
